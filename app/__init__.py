a0a20636356d2b310a87e12b48e291d43ef92002
kuldeep behera <kuldeepbehera93@gmail.com>
2025-09-13 12:31:17 +0530

    Fix Flask app deployment on Render: add api/__init__.py, revert to absolute import, update gunicorn command

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from ..config import Config
+from config import Config
 from .api import api_bp
 import logging
 
